b9c8d387934d48c4399ad6cc33ba8bec5e75c09c
Kalr Ma <wwwhat1101@gmail.com>
2022-04-07 20:06:05 -0700

    add code to get useful data

--- riot_api.py
+++ riot_api.py
@@ -29,8 +29,39 @@ test = watcher.league.entries("na1","RANKED_SOLO_5x5","DIAMOND","II",1)
 f = open("test2.txt", "a",encoding='utf-8')
 # f.truncate(0)
 for t in range(150):
-    f.write(watcher.summoner.by_name(my_region,test[t]["summonerName"])["puuid"]+'||'+test[t]["summonerName"]+'\n')
+    f.write(watcher.summoner.by_name(
+        my_region,test[t]["summonerName"])["puuid"]+
+        '||'+test[t]["summonerName"]+'\n')
 f.close()
+
+# get puuid 
+test_puuid = watcher.summoner.by_name(my_region,'UnsungSoul')['puuid']
+
+# use puuid to get a list of games that player has played
+watcher.match.matchlist_by_puuid('AMERICAS',
+'UmFDyJV9IIWfPwI-T3jZccdgKBet0e8KsoEFSWjEEjQUznmk4pgqJI_9fNuGyLHfPFc_3nj9GU5XXQ',
+count=10)
+
+# get single game information by that player
+temp = watcher.match.by_id('AMERICAS','NA1_4266324078')['info']['participants']
+
+len(temp)
+
+type(temp[1])
+temp[1].get('assists')
+# some useful data from the api will add more 
+key_value = ['assists','baronKills','bountyLevel','champExperience','champLevel','championName',
+'consumablesPurchased','damageDealtToBuildings','damageDealtToObjectives','damageDealtToTurrets',
+'damageSelfMitigated', 'deaths', 'detectorWardsPlaced', 'goldEarned','goldSpent','kills',
+'lane', 'largestCriticalStrike', 'largestKillingSpree', 'largestMultiKill', 'longestTimeSpentLiving',
+'neutralMinionsKilled','role','summonerLevel','win','visionScore','visionWardsBoughtInGame','wardsKilled','wardsPlaced']
+
+# get useful data for player
+for i in range(len(temp)):
+    if(temp[i]['summonerName']=='UnsungSoul'):
+        [temp[i].get(k) for k in key_value]
+
+
 
 # check the type of returned frame
 type(test)
